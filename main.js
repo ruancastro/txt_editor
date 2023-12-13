f4275237bd92fdc9e118a714d4d0d085a2b93f7c
Ruan Castro <ruancastro15@gmail.com>
2023-12-13 20:12:26 -0300

    Adding a menu and submenu on top bar, without funcs

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu } = require('electron');
 // MAIN WINDOW
 var mainWindow = null;
 async function createWindow(){
@@ -9,6 +9,28 @@ async function createWindow(){
     });
     await mainWindow.loadFile('src/pages/editor/index.html');
 }
+
+// TEMPLATE MENU
+const templateMenu = [
+    {
+        label:  'Arquivo',
+        submenu: [
+            {
+                label:  'Novo'
+            },
+            {
+                label:  'Abrir'
+            },
+            {
+                label:  'Salvar'
+            }
+        ]
+    },
+];
+
+// Menu
+const menu = Menu.buildFromTemplate(templateMenu);
+Menu.setApplicationMenu(menu);
 
 // ON READY
 app.whenReady().then(createWindow);
